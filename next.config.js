26be02056b00a61f0f10d83c4496299305482a9f
dce23 <dce23@elepki@hotmail.com>
2021-09-26 01:29:59 -0600

    Built the map component getCenter from geolib and added a push pin when clicked shows the description of the location

--- next.config.js
+++ next.config.js
@@ -2,4 +2,8 @@ module.exports = {
   images: {
     domains: ["links.papareact.com", "jsonkeeper.com", "a0.muscache.com"],
   },
+  env: {
+    mapbox_key:
+      "pk.eyJ1IjoiZGNlMjMiLCJhIjoiY2t0bnljNjFqMDdwYTJ2bnR2dXpkdXdiZCJ9.1ZKw0ecXOXjn8PCF0y7RzQ",
+  },
 };
